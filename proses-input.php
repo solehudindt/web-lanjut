17241823b4c3ab9af5c28c47acbb8ef261fb6b53
Solehudindt <41879176+solehudindt@users.noreply.github.com>
2019-03-23 19:30:50 +0700

    Update proses-input.php

--- proses-input.php
+++ proses-input.php
@@ -4,19 +4,14 @@
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $lastupdate = $_POST['lastupdate'];
-    // $sql = "INSERT INTO actor(first_name, last_name, last_update) VALUES(:first_name, :last_name, :last_update)";
+
     $sql = "INSERT INTO actor(first_name, last_name, last_update) VALUES(:first_name, :last_name, now() )";    
 
-    //var_dump($firstname);
-    //var_dump($lastname);
-    //var_dump($lastupdate);
     $query = $conn->prepare($sql);
 
-    //$query->bindParam(':actor_id')
     $query->bindParam(':first_name',$firstname);
     $query->bindParam(':last_name',$lastname);
-    //$query->bindParam(':last_update',$lastupdate);
-
+    
     $query->execute();
 
     header("location:input.php");
